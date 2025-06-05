10ffe350200c0b52163251e091380e62e345a5d0
tndwyntlnt <tandwiyan9906@gmail.com>
2025-06-05 13:55:30 +0700

    fourth commit - nambahin chart user ke dashboard admin

---
+++ app/Filament/Widgets/UserChart.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+use App\Models\User;
+
+class UserChart extends ChartWidget
+{
+    protected static ?string $heading = 'Chart';
+
+    protected static string $color = 'success';
+
+    protected function getData(): array
+    {
+        $data = Trend::model(User::class)
+            ->between(
+                start: now()->startOfMonth(),
+                end: now()->endOfMonth(),
+            )
+            ->perDay()
+            ->count();
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'User Terdaftar',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'line';
+    }
+}
